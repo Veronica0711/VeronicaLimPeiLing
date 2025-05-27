661af7239adb6f2933e69925b948d761e98c7c3c
Veronica0711 <veronicalim81@gmail.com>
2025-05-27 14:23:57 +0800

    Assignment part 1
    1 and 2 completed

--- Assignment_4/question1.js
+++ Assignment_4/question1.js
@@ -2,7 +2,11 @@ const amdPrices = [90.21, 89.15, 88.15, 87.12, 102.21, 100.12, 79.82];
 
 // Write your code below
 let highest = amdPrices[0]; 
-for(let i = 0; i < amdPrices.length; i++) {
-  if (amdPrices[i] > highest); 
+
+for (let i = 0; i < amdPrices.length; i++) {
+  if (amdPrices[i] > highest) {
+    highest = amdPrices[i]; 
+  }
 }
-console.log("AMD 52-week high is", highest);
+
+console.log("The highest stock price is", highest);
--- Assignment_4/question2.js
+++ Assignment_4/question2.js
@@ -1,3 +1,5 @@
 const amdPrices = [90.21, 89.15, 88.15, 87.12, 102.21, 100.12, 79.82];
 
 // Write your code below
+let averagePrice = amdPrices.reduce((sum, price) => sum + price, 0) / amdPrices.length;
+console.log(`The 7-day SMA of AMD is $${averagePrice.toFixed(2)}`);
