28f89544f5bd7be2312081709baf6515635bd7e6
Veronica0711 <veronicalim81@gmail.com>
2025-05-28 16:18:05 +0800

    Final Work_Assignment 4
    Completion 28th May

--- Assignment_4/question6.js
+++ Assignment_4/question6.js
@@ -2,7 +2,8 @@
 
 // const amdPrices = [90.21, 89.15, 88.15, 87.12, 102.21, 100.12, 79.82]
 
-let data = ()
-for price in data:
-	if price < 3:
-    data.push(price)
+const amdPrices = [90.21, 89.15, 88.15, 87.12, 102.21, 100.12, 79.82];
+const sortedPrices = amdPrices.slice().sort((a, b) => a - b);
+const lowestThree = sortedPrices.slice(0, 3);
+
+console.log("The 3 lowest average prices for AMD are", lowestThree);
